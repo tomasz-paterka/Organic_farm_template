33f82764c9bdfcfd607fbc742a71a9e2915d16bd
Tomasz Paterka <tomekpaterka@gmail.com>
2019-03-29 22:17:18 +0100

    add npm packages, webpack basic configuration

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    main: './src/js/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].[hash].js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "../src"),
+    open: true,
+    hot: true
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash()
+  ]
+};
